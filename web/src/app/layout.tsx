ee6efbf0ddc87c194f050624e78596da4c3cdee6
Letícia Aragão <leticia.aragao2208@gmail.com>
2023-05-22 22:56:29 -0300

    feat: layout and logout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,22 +1,65 @@
-import './globals.css'
-import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree, Roboto_Flex } from 'next/font/google'
+import React from "react";
+import { Profile } from "@/components/Profile";
+import "./globals.css";
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from "next/font/google";
+import { Hero } from "@/components/Hero";
+import { Copyright } from "@/components/Copyright";
+import { cookies } from "next/headers";
+import { Signin } from "@/components/Signin";
 
-const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
-const baiJamjuree = BaiJamjuree({ subsets: ['latin'], weight: '700', variable: '--font-bai-jamjuree' })
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
+const baiJamjuree = BaiJamjuree({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-bai-jamjuree",
+});
 
 export const metadata = {
-  title: 'NLW Spacetime',
-  description: 'Uma capsula do tempo construida com nextjs, tailwind e typescript',
-}
+  title: "NLW Spacetime",
+  description:
+    "Uma capsula do tempo construida com nextjs, tailwind e typescript",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const isAuthenticated = cookies().has("token");
+
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans text-gray-100 bg-gray-900`}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* blur */}
+            <div className="absolute right-0 top-1/2 h-[280px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/* stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
+
+            {/* sign-in */}
+            {isAuthenticated ? <Profile /> : <Signin />}
+
+            {/* hero */}
+            <Hero />
+
+            {/* Copyright */}
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
-  )
+  );
 }
