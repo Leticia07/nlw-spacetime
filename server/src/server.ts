7ad85d8db8af5cb0eb5d38fe61a401d2344c92f7
Letícia Aragão <leticia.aragao2208@gmail.com>
2023-05-23 23:01:39 -0300

    feat: file upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,8 +5,18 @@ import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { authRoutes } from './routes/auth'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+})
 
 app.register(cors, {
   origin: ['https://localhost:3000']
@@ -17,6 +27,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
